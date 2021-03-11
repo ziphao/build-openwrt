265d990c9f6109dbda563a79fbde4abedc07fdcc
279437541 <78850005+279437541@users.noreply.github.com>
2021-03-11 20:46:12 +0800

    Update diy-2.sh

--- build/Project_nanopi_r2s/diy-2.sh
+++ build/Project_nanopi_r2s/diy-2.sh
@@ -6,7 +6,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-dahuilang'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-DaHuiLang'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（dahuilang build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/ImmortalWrt 18.06 /大灰狼 build $(TZ=UTC-8 date "+%Y.%m.%d") @ ImmortalWrt 18.06 /g" package/lean/default-settings/files/zzz-default-settings
